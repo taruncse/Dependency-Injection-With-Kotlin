0f865cb3e2c770109328f832fc7fefe75fca63ce
Tarun <tarun.pstu@gmail.com>
2019-06-10 14:32:02 +0600

    init

---
+++ app/src/main/java/com/tkb/dependency_injection_with_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tkb.dependency_injection_with_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
