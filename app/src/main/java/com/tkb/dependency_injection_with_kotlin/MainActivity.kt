9537d8f0ff0d95eb05b5aec4b90692aed6eefad5
Tarun <tarun.pstu@gmail.com>
2019-06-14 17:49:08 +0600

    # Added Message Activity # Updated gradle dependency

--- app/src/main/java/com/tkb/dependency_injection_with_kotlin/MainActivity.kt
+++ app/src/main/java/com/tkb/dependency_injection_with_kotlin/MainActivity.kt
@@ -2,11 +2,14 @@ package com.tkb.dependency_injection_with_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.File.separator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/tkb/dependency_injection_with_kotlin/MessageActivity.java
@@ -0,0 +1,13 @@
+package com.tkb.dependency_injection_with_kotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
